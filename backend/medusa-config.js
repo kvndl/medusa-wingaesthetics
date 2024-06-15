019a314eeeeb6d44fa15d7cc4a4882eacc5b1cd6
Noah Schepers <noah.schepers@veridos.com>
2024-06-15 02:44:20 -0400

    THE ONE THAT CHANGED IT ALL

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -48,7 +48,7 @@ const plugins = [
     resolve: `medusa-payment-stripe`,
     options: {
       api_key: process.env.STRIPE_API_KEY,
-      // webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
     },
   },
   {
