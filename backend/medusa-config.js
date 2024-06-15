5a03a0c260b5f08f2d33f407ebf5aa413cf2d609
Noah Schepers <noah.schepers@veridos.com>
2024-06-15 06:30:27 -0400

    auto-payment methods :D

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -52,6 +52,7 @@ const plugins = [
     options: {
       api_key: process.env.STRIPE_API_KEY,
       webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+      automatic_payment_methods: true
     },
   },
   {
